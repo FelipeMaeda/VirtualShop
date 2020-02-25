878a3c8d1f3613977859492a19d50890f3a5a727
FelipeMaeda <maedaxd@gmail.com>
2020-02-25 06:41:51 -0300

    Subindo Styles e JS do bootstrap

--- mymarket/website/views.py
+++ mymarket/website/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def helloWorld(request):
+    return HttpResponse('teste')
+
+def home(request):
+    return render(request, 'website/index.html')
 
 # Create your views here.
